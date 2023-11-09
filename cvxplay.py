4fe4c8ad99dfbdfd5e6aae88907c0bd6d7fd8884
MrLatinNerd <minghuayin1729@gmail.com>
2023-11-09 00:57:24 -0800

    Still trying to fix the convex optimization code...

--- cvxplay.py
+++ cvxplay.py
@@ -1,41 +1,22 @@
+import math
 import cvxpy as cp
 import numpy as np
 
-# Fix random number generator so we can repeat the experiment.
-np.random.seed(1)
+A = np.array([[1, 1, -1.414213562, 0, 0, 0],
+              [1, -1, 0, -1.414213562, 0, 0],
+              [1, 1j, 0, 0, -1.414213562, 0],
+              [1, -1j, 0, 0, 0, -1.414213562]]).T
 
-# The threshold value below which we consider an element to be zero.
+b = np.array([1, 0.707106781 + 0.707106781j, 0, 0, 0, 0])
 
-A = [[-1, -1, 0.707106781, 0, 0, 0], [-1, 1, 0, 0.707106781, 0, 0],
-     [-1, -1j, 0, 0, 0.707106781, 0], [-1, 1j, 0, 0, 0, 0.707106781]]
+x_l1 = cp.Variable(4, complex=True)
 
-b = [1, 0.707106781 + 0.707106781j, 0, 0, 0, 0]
+obj = cp.Minimize(cp.norm(A@x_l1 + b, 1))
 
-# Create variable.
-x_l1 = cp.Variable(4)
-ll = 1
-
-A_real = []
-A_im = []
-for v in A:
-    A_real.append(np.real(v))
-    A_im.append(np.imag(v))
-
-
-# Create constraint.
-#constraints = [A@x_l1 - b]
-#constraints +=[A@x_l1 +b>= -0.001]
-
-constraints = []
-
-obj = cp.Minimize(cp.norm(A_real@x_l1 + A_im@x_l1 - b, 2) +
-                  ll*(cp.norm(x_l1, 1)))
-
-#prob = cp.Problem(obj, constraints)
 prob = cp.Problem(obj)
 solution = prob.solve()
-print("status: {}".format(prob.status))
+print(f"status: {prob.status}")
 
-#nnz_l1 = (np.absolute(x_l1.value) > delta).sum()
-print("optimal objective value: {}".format(obj.value))
-print(x_l1.value)
+print(f"optimal objective value: {obj.value}")
+print(repr(x_l1.value))
+print(f'{4/(2 + math.sqrt(2)) = }')
