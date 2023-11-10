8e7528bb870ff1bfe91117a9f0ab62124d964c93
MrLatinNerd <minghuayin1729@gmail.com>
2023-11-09 16:47:04 -0800

    Memory issues for n=5 :(

--- cvx_opti.py
+++ cvx_opti.py
@@ -16,7 +16,7 @@ def ham(n: int) -> int:
     return weight
 
 
-def optimize_stab_extent_T(n: int):
+def optimize_stab_extent_T(n: int, print_output=False):
     # B = np.loadtxt('data/1_qubit_B.csv', dtype=complex, delimiter=',')
     B = spr.load_npz(f'data/{n}_qubit_B.npz')
     num_stab_states, num_non_comp_stab_states = B.shape
@@ -32,15 +32,19 @@ def optimize_stab_extent_T(n: int):
 
     prob = cp.Problem(obj)
     solution = prob.solve()
-    print(f"status: {prob.status}")
 
-    print(f"optimal objective value: {obj.value}")
-    # print(repr(x_l1.value))
-    print(f'{(4/(2 + math.sqrt(2)))**n = }')
+    if print_output:
+        print(f"status: {prob.status}")
+
+        print(f"optimal objective value: {obj.value}")
+        # print(repr(x_l1.value))
+        print(f'{(4/(2 + math.sqrt(2)))**n = }')
 
     return B, obj.value, x_l1.value
 
 
 if __name__ == '__main__':
     # B_old = np.loadtxt('data/1_qubit_B.csv', dtype=complex, delimiter=',')
-    B, optimal_val, x = optimize_stab_extent_T(3)
+    start = time.perf_counter()
+    B, optimal_val, x = optimize_stab_extent_T(5, True)
+    print(f'Time elapsed: {time.perf_counter() - start}')
