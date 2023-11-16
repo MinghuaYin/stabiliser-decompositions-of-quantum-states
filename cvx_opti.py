abce349ea18726468f5564cc88e4a2e846999733
MrLatinNerd <minghuayin1729@gmail.com>
2023-11-15 23:01:32 -0800

    helper_functions 🤢

--- cvx_opti.py
+++ cvx_opti.py
@@ -8,7 +8,8 @@ import numpy as np
 import scipy.sparse as spr
 
 from F2_helper.F2_helper import fast_log2, int_to_array
-from itertools import combinations
+from functools import reduce
+from itertools import combinations, product
 from stabiliser_state.Check_Matrix import Check_Matrix
 from typing import Tuple
 
@@ -30,11 +31,6 @@ def T_state(n) -> np.ndarray:
 
 
 def dicke_state(n, weight) -> np.ndarray:
-    """
-    Note: Dicke state is not normalized --- all nonzero elements are 1
-
-    """
-
     dicke_state = np.zeros(1 << n, dtype=np.int8)
     combos = combinations(range(n), weight)
 
@@ -43,19 +39,28 @@ def dicke_state(n, weight) -> np.ndarray:
         index = int(index_str, 2)
         dicke_state[index] = 1
 
-    return dicke_state
+    return dicke_state / np.linalg.norm(dicke_state)
 
 
 def CCZ_state(n) -> np.ndarray:
-    return np.array([1]*((1 << n)-1) + [-1], dtype=np.int8)
+    return 1/2**(n/2) * np.array([1]*((1 << n)-1) + [-1], dtype=np.int8)
 
 
-def optimize_stab_extent_T(n: int, print_output=False, solver='GUROBI') -> Tuple[np.ndarray, float, np.ndarray]:
+def W_state(n) -> np.ndarray:
+    def g(x: Tuple[int]):
+        return reduce(lambda a, b: a ^ b, x) + sum(x)
+
+    return 1/2**(n/2) * np.array([np.exp(1j * math.pi * g(x)/4)
+                                  for x in product((0, 1), repeat=n)], dtype=complex)
+
+
+def optimize_stab_extent(state: np.ndarray, n: int, print_output=False, solver='GUROBI') -> \
+        Tuple[np.ndarray, float, np.ndarray]:
     B = spr.load_npz(f'data/{n}_qubit_B.npz')
     num_stab_states, num_non_comp_stab_states = B.shape
 
     c = np.zeros(num_stab_states, dtype=complex)
-    c[:(1 << n)] = T_state(n)
+    c[:(1 << n)] = state
     c = spr.csc_array(c).reshape((num_stab_states, 1))
 
     x_l1 = cp.Variable((num_non_comp_stab_states, 1), complex=True)
@@ -83,7 +88,8 @@ def optimize_stab_extent_T(n: int, print_output=False, solver='GUROBI') -> Tuple
     return B, obj.value, x_l1.value
 
 
-def more_precise_soln(n: int, x: np.ndarray, non_stab_state: np.ndarray, rnd_dec: int = 3) -> Tuple[float, np.ndarray, np.ndarray]:
+def more_precise_soln(n: int, x: np.ndarray, non_stab_state: np.ndarray, rnd_dec: int = 3) -> \
+        Tuple[float, np.ndarray, np.ndarray]:
     with open(f'data/{n}_qubit_subgroups.data', 'rb') as reader:
         xmatrs = pickle.load(reader)
 
@@ -119,6 +125,6 @@ if __name__ == '__main__':
 
     print(f'{n = }')
     start = time.perf_counter()
-    B, optimal_val, x = optimize_stab_extent_T(n, True, solver)
+    B, optimal_val, x = optimize_stab_extent(n, True, solver)
     np.save(f'data/{n}_qubit_soln', x)
     print(f'Time elapsed: {time.perf_counter() - start}')
