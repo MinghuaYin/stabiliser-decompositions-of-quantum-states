238fef4a21c9dc633a953b74411a3aba8eb148a6
MrLatinNerd <minghuayin1729@gmail.com>
2023-11-27 11:26:37 -0800

    Add multiprocessing to get_dict_form_data()

--- helper_functions_slurm.py
+++ helper_functions_slurm.py
@@ -2,20 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import gc
-import math
-import numba
 import pickle
-import sys
-import time
 
 import F2_helper.F2_helper as f2
 import multiprocessing as mp
-import numpy as np
 import scipy.sparse as spr
 
-from functools import reduce
-from itertools import count, product
-from typing import List, Tuple
+from typing import List
+from io import BufferedReader
 
 from helper_functions import *
 
@@ -56,13 +50,26 @@ def get_B_data():
     print('get_B_data finished')
 
 
-def update_data(data: List, hash_map: dict):
+def update_data(args):
+    data, hash_map = args
+    updated_data = []
+
     for col_num, child1, child2, rel_phase in data:
         child1_index = hash_map[str(child1[:, :-1])] * (1 << n) \
             + f2.array_to_int(child1[:, -1])
         child2_index = hash_map[str(child2[:, :-1])] * (1 << n) \
             + f2.array_to_int(child2[:, -1])
-        yield (col_num, child1_index, child2_index, rel_phase)
+        updated_data.append((col_num, child1_index, child2_index, rel_phase))
+
+    return updated_data
+
+
+def args(reader: BufferedReader, hash_map: dict):
+    try:
+        while True:
+            yield (pickle.load(reader), hash_map)
+    except EOFError:
+        pass
 
 
 def get_dict_form_data():
@@ -71,27 +78,20 @@ def get_dict_form_data():
 
     """
 
-    # TODO Multiprocessing?
     with open(f'data/{n}_qubit_hash_map{tail}.data', 'rb') as r1, \
-            open(f'data/{n}_B_data{tail}.data', 'rb') as r2:
+            open(f'data/{n}_B_data{tail}.data', 'rb') as r2, \
+            mp.Pool() as pool:
         hash_map = pickle.load(r1)
-        partial_data = []
-        try:
-            while True:
-                partial_data.extend(pickle.load(r2))
-        except EOFError:
-            pass
+        print(f'{len(hash_map) = }')
 
-    print(f'{len(hash_map) = }')
-    print(f'{len(partial_data) = }')
-
-    big_dict = {}
-
-    for col_num, child1_index, child2_index, rel_phase \
-            in update_data(partial_data, hash_map):
-        big_dict[(child1_index, col_num)] = 1
-        big_dict[(child2_index, col_num)] = rel_phase
-        big_dict[(col_num + (1 << n), col_num)] = -sqrt2
+        big_dict = {}
+        results = pool.imap_unordered(
+            update_data, args(r2, hash_map), chunksize=20)
+        for partial_data in results:
+            for col_num, child1_index, child2_index, rel_phase in partial_data:
+                big_dict[(child1_index, col_num)] = 1
+                big_dict[(child2_index, col_num)] = rel_phase
+                big_dict[(col_num + (1 << n), col_num)] = -sqrt2
 
     with open(f'data/{n}_qubit_dict_form{tail}.data', 'wb') as w:
         pickle.dump(big_dict, w)
@@ -114,6 +114,6 @@ def from_dict_form_data(num_of_stab_states):
 
 
 if __name__ == '__main__':
-    get_B_data()
-    # get_dict_form_data()
+    # get_B_data()
+    get_dict_form_data()
     # from_dict_form_data(315057600)
